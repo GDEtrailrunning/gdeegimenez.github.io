c5d3f4c3a7ccb4c9fecbeccd441b56aed267eb31
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-08 13:04:35 -0300

    Subo semana 10 y 11

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 6',
-    data: [10, 13, 10, 10, 14], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 10',
+    data: [10, 18, 11, 5, 13], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 7',
-    data: [7, 6, 9, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 11',
+    data: [7, 6, 9, 30], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -53,7 +53,7 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
 
 function actualizarDiasRestantes() {
   // Fecha objetivo fija
-  const objetivoDate = new Date('2026-02-21');
+  const objetivoDate = new Date('2026-03-21');
   const currentDate = new Date();
   const timeDiff = objetivoDate - currentDate;
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
