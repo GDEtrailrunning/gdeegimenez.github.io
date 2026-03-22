f4d452416b423a155297825f902228dc2e6cb725
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-22 13:25:09 -0300

    Subo semana 12 y 13

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 10',
-    data: [10, 18, 11, 5, 13], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 12',
+    data: [6, 7, 5, 4, 30], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 11',
-    data: [7, 6, 9, 30], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 13',
+    data: [8, 10, 10, 17, 23], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -53,7 +53,7 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
 
 function actualizarDiasRestantes() {
   // Fecha objetivo fija
-  const objetivoDate = new Date('2026-03-21');
+  const objetivoDate = new Date('2026-03-29');
   const currentDate = new Date();
   const timeDiff = objetivoDate - currentDate;
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
